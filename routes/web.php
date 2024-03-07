463ba03f3ccf40d3a722652b2b76f1ffe47a4c39
noppadolr <riyasarn.n@gmail.com>
2024-03-07 17:44:26 +0700

    Applying Routing Middleware

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,14 @@ use App\Livewire\Logout;
 // });
 
 
-Route::get('/',Login::class)->name('user.login');
-
-
-
-Route::get('register',Register::class)->name('user.register');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('home',Home::class)->name('admin.home');
     Route::get('logout',Logout::class)->name('logout');
+});
+
+
+Route::group(['middleware' => 'checkUserLoogedIn'], function () {
+    Route::get('/',Login::class)->name('user.login');
+    Route::get('register',Register::class)->name('user.register');
 });
